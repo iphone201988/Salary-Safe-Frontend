32a311f043832ecff53b0fc42494d075348a56d9
Pardeep Singh <iphone201988@gmail.com>
2024-11-12 11:37:35 +0530

    fix

--- src/components/Dashboard/DashboardSummary/CardShowDashboard.tsx
+++ src/components/Dashboard/DashboardSummary/CardShowDashboard.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 interface TotalSalaryProps {
   title: string;
   data: string;
-  percentage: string;
+  percentage?: string | undefined;
 }
 
 const CardShowDashboard: React.FC<TotalSalaryProps> = ({ title, data, percentage }) => {
@@ -11,12 +11,12 @@ const CardShowDashboard: React.FC<TotalSalaryProps> = ({ title, data, percentage
     <div className="p-4 bg-white rounded-lg shadow-md text-center">
       <h2 className="text-lg font-medium">{title}</h2>
       <p className="text-3xl font-bold">{data}</p>
-      <span className="text-sm text-gray-500">
-        <span className={parseFloat(percentage) < 0 ? 'text-red-500' : 'text-green-500'}>
+      {percentage &&<span className="text-sm text-gray-500">
+       <span className={parseFloat(percentage) < 0 ? 'text-red-500' : 'text-green-500'}>
           {percentage}%
         </span>{' '}
         vs Previous Month
-      </span>
+      </span>}
     </div>
   );
 };
--- src/components/Dashboard/DashboardSummary/HrDashboard.tsx
+++ src/components/Dashboard/DashboardSummary/HrDashboard.tsx
@@ -5,24 +5,30 @@ import EmployeeContractType from './EmployeeContractType';
 import CardShowDashboard from './CardShowDashboard';
 import EmploymentStatus from './EmploymentStatus';
 import HiredVsLeftChart from './HiredVsLeftChart';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../Redux/store';
 
 const HrDashboard: React.FC = () => {
+    const { employeerDetails } = useSelector((state: RootState) => state.user);
+
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-semibold text-center mb-4">HR Dashboard</h1>
+      <h1 className="text-2xl font-semibold text-center mb-4">Employer Dashboard</h1>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mb-6">
-        <CardShowDashboard title='Total Salary (YTD)' data='3,360,000' percentage='0.5' />
-        <CardShowDashboard title='Average Salary' data='25,560' percentage='-1.9' />
-        <CardShowDashboard title='Turnover Rate' data='2.1%' percentage='0.0' />
-        <CardShowDashboard title='Average Age' data='3,360,000' percentage='-0.5' />
-        <CardShowDashboard title='Parameter Rate' data='90%' percentage='-0.5' />
-        <CardShowDashboard title='Absenteeism Rate' data='2.1%' percentage='25' />
+        <CardShowDashboard title='Job Title' data="Devlovper" />
+        <CardShowDashboard title='Employment Type' data='Full-Time'  />
+        <CardShowDashboard title='Date' data={new Date().toISOString().split("T")[0]} />
+        <CardShowDashboard title='Location and currency' data='Mohali,punjab'/>
+        <CardShowDashboard title='Your Salary Range' data='$500000' />
       </div>
-        <EmployeeContractType />
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 ">
         <EmployeesChart />
         <SalaryChart />
+       
+        </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         <EmploymentStatus />
+        <EmployeeContractType />
         <HiredVsLeftChart />
       </div>
     </div>
